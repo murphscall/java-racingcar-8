7179bdb01108315dcaa3215a8307d308358b3491
kimjinhoo <wlsgnwkd22@naver.com>
2025-10-23 04:32:47 +0900

    feature(tryCount) : 시도 횟수 입력 기능 구현 - 시도 횟수 입력에 숫자가 아닌 값이 들어올 경우 예외 처리

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -8,4 +8,15 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public static int tryCount() {
+        System.out.println("게임을 얼마나 몇 번 진행할까요?");
+        String input = Console.readLine();
+
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자를 입력해주세요");
+        }
+    }
 }
